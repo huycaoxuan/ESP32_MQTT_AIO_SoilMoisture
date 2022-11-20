74bc742e46abaa05e1c4129d39559cfc64e566fc
Huy Cao <33714452+huycaoxuan@users.noreply.github.com>
2022-11-20 07:47:03 +0700

    Bo sung file setup

---
+++ secrets.h
@@ -0,0 +1,10 @@
+// Use this file to store all of the private credentials 
+// and connection details     
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "xxx"
+#define AIO_KEY         "xxx" // Put Your adafruit ID here
+#define WIFI_SSID01      "agrihn02"      //ID de setup wifi khi chua ket noi
+#define WIFI_SSID02     "OnDemandAP"     //ID config wifi
+#define WIFI_PW         "YourPassWord"      //PW
